28a97d8973db0783e542e4364ad441d2b27a15a1
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-09-10 12:56:04 +0300

    localstorage updated

--- src/redux/reducers/cartReducers.jsx
+++ src/redux/reducers/cartReducers.jsx
@@ -8,7 +8,7 @@ import {
 const initialState = {
     cart: []
   };
-  initialState.cart=[...JSON.parse(localStorage.getItem('cart'))]
+  initialState.cart=JSON.parse(localStorage.getItem('cart'))
 export const cartReducer = (state=initialState,{type,payload})=>{
     switch (type) {
         case CART:
